2e623b872eb0ee632dd069e3aab2aa94e531a23f
MKS <kamransadique@gmail.com>
2020-07-15 10:53:44 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:alpine as buildstage
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+FROM nginx
+COPY --from=buildstage /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey! Nice to see you up and running!
         </p>
         <a
           className="App-link"
